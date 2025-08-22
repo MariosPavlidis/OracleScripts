8520b4f7529e3dc4a9a9472e8d7aa5fcf9634e52
MariosPavlidis <pavlidis.marios@gmail.com>
2025-08-22 12:14:23 +0300

    Update active_sessions.sql

--- Perfomance/active_sessions.sql
+++ Perfomance/active_sessions.sql
@@ -1,18 +1,16 @@
-
-SELECT
-    ''''||s.sid||','||s.serial#||'@'||s.inst_id||'''' sid,
+SELECT   ''''||s.sid||','||s.serial#||',@'||s.inst_id||'''' sid,
     s.status,
     s.sql_id,
+    s.event AS current_wait,
+    s.wait_time,
+    s.seconds_in_wait,
     s.sql_child_number,
     s.sql_exec_start,
     ROUND((SYSDATE - s.sql_exec_start) * 86400) AS elapsed_seconds,
     s.username,
     s.osuser,
     s.machine,
-    s.program,
-    s.event AS current_wait,
-    s.wait_time,
-    s.seconds_in_wait,
+    s.program,    
     q.plan_hash_value,
     COALESCE(q.sql_patch, q.sql_profile, q.sql_plan_baseline, 'NONE') AS patch_profile_baseline,
     q.sql_fulltext
