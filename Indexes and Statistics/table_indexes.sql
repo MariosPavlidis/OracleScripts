a399139d0b0fc77d1c26332097e6d4ce315e8e33
MariosPavlidis <pavlidis.marios@gmail.com>
2025-09-25 13:16:51 +0300

    fix multi owner

--- Indexes and Statistics/table_indexes.sql
+++ Indexes and Statistics/table_indexes.sql
@@ -1,7 +1,9 @@
 with IDX as (
 Select b.table_owner,a.table_name, a.index_name, a.column_name||' '||decode(a.descend,'DESC','DESC') column_name,column_position,
 b.uniqueness,distinct_keys,blevel,leaf_blocks,num_rows,status,degree,global_stats,partitioned
-FROM dba_ind_columns a,dba_indexes b where b.index_name = a.index_name
+FROM dba_ind_columns a,dba_indexes b 
+where b.index_name = a.index_name
+and b.table_owner= a.table_owner
 and a.table_name=:1
 order by 1,2,3,5
 )
