f928a589fcc32c5f99f2a3c386b5c67f5b1e7efd
MariosPavlidis <pavlidis.marios@gmail.com>
2025-10-07 12:34:40 +0300

    Update StatistιcsGatherExecutions.sql

--- Indexes and Statistics/StatistιcsGatherExecutions.sql
+++ Indexes and Statistics/StatistιcsGatherExecutions.sql
@@ -2,3 +2,10 @@ SELECT instance_id,JOB_NAME,ACTUAL_START_DATE,RUN_DURATION,STATUS
 FROM DBA_SCHEDULER_JOB_RUN_DETAILS
 WHERE JOB_NAME like '%OPT%'
 order by ACTUAL_START_DATE desc;
+
+
+select *
+from dba_autotask_job_history
+where client_name='auto optimizer stats collection'
+and (end_time is null or job_status<>'SUCCEEDED')
+order by start_time desc;
